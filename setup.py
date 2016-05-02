97a7777058bea15f00fb6f65cfd331a735a90f06
David Sanders <dsanders11@ucsbalum.com>
2016-05-01 17:45:26 -0700

    Another attempt at the docstring fix

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ For older changes, see
 
 from setuptools import setup, find_packages
 
-doclines = (__doc__ or '').splitlines()
+doclines = (__doc__ or '').splitlines() or ['', '']
 
 setup(name="python-card-me",
       version="0.9.3",
